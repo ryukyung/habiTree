d65a694abf611b307b1b84882dfe0be77dcfaeeb
ryukyunghye <ryukyung123@naver.com>
2023-06-21 23:31:22 +0900

    :lipstick: (feat/#7): 모달 컴포넌트 생성

---
+++ src/components/modal/SelectDate.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import common from '../../styles/common';
+
+export default function SelectDate({ placeholder }) {
+  return (
+    <SelectDateCover>
+      <input
+        type="date"
+        name="start-date"
+        data-placeholder={placeholder}
+        required
+        aria-required="true"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </SelectDateCover>
+  );
+}
+const SelectDateCover = styled.div`
+  position: relative;
+  input {
+    all: unset;
+    width: 108px;
+    padding-left: 20px;
+  }
+  input[type='date']::before {
+    content: attr(data-placeholder);
+    width: 100%;
+  }
+  input[type='date']:focus::before,
+  input[type='date']:valid::before {
+    display: none;
+  }
+  input[type='date']::-webkit-calendar-picker-indicator {
+    background-image: url('/src/assets/svg/calendar.svg');
+    background-size: cover;
+    width: 14px;
+    height: 14px;
+    position: absolute;
+    left: 0;
+  }
+`;
